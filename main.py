87803e787b70b4dc1c1a2ec71cfe3a75bef810ef
M̸̢̠̘̣̦̩̦̮̰̤̅͘͝ạ̵̯̠̫͓̖̲͈̰̬̈̈͆̂̈́́̌͂̚d̸̝̰̬͇̝̲̬̪͊͂̀͂͗h̴̬͔͚͊͑͘͝ǘ̷̗̙͚̪̂̐̓̈́̄̄̑͜v̴̙̊̀̽̄̎̌̀̿͜͝ ̵̧̳̜̮̜̝̈́̈́͘S̵̩̻̜̍́̏̿h̸̪̲͍̺͍̠̀͊̑a̸̧̱̩̙͚͔̻̙̎̓̋̓͝r̴̢̢̛̻͗̎͗͑̊m̷̨̖͎̞͖͛͊̓̕à̷̧̨̨̙̮͕̫̟̲̇̔͒͗̇͊̉ <31816381+madhuv-sharma@users.noreply.github.com>
2021-06-24 13:40:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import nltk
-# nltk.download('stopwords')
-# nltk.download('wordnet')
-from numpy import reshape
-#import pandas as pd
+nltk.download('stopwords')
+nltk.download('wordnet')
 import re
 import six.moves.cPickle as pickle
-#import spacy
-#from nltk import NaiveBayesClassifier as nbc
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-#from sklearn.preprocessing import LabelEncoder
 
 def getRawText():
 	while True:
@@ -22,21 +16,6 @@ def getRawText():
 			exit()
 		else :
 			return raw_text
-
-'''
-def removeApostrophe(text):
-	phrase = re.sub(r"won't", "will not", text)
-	phrase = re.sub(r"can\'t", "can not", text)
-	phrase = re.sub(r"n\'t", " not", text)
-	phrase = re.sub(r"\'re", " are", text)
-	phrase = re.sub(r"\'s", " is", text)
-	phrase = re.sub(r"\'d", " would", text)
-	phrase = re.sub(r"\'ll", " will", text)
-	phrase = re.sub(r"\'t", " not", text)
-	phrase = re.sub(r"\'ve", " have", text)
-	phrase = re.sub(r"\'m", " am", text)
-	return phrase
-'''
 
 def removeSpecialChars(text):
 	return re.sub('[^a-zA-Z]', ' ', text)
@@ -59,10 +38,6 @@ def getScore(text):
 	train = pickle.load(f)
 	f.close()
 	X_test=[text]
-	#cv=CountVectorizer(ngram_range=(1,2))
-	#X_vec=cv.fit_transform(X_train).toarray()
-	#li = list(text.split(" "))
-	#text_vec=cv.fit_transform(li).toarray()
 	vectorizer = CountVectorizer()
 	train_features = vectorizer.fit_transform([t for t in train[0]])
 	Xt_vec = vectorizer.transform(X_test).toarray()
